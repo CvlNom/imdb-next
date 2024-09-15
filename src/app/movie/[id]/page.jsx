5e844e761b8895b579b3ffd5426eb190edfd07dc
CvlNom <alexkwak24@gmail.com>
2024-09-15 21:09:46 +0900

    small changes

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -9,7 +9,7 @@ export default async function MoviePage({ params }) {
   //   console.log(movie)
 
   return <div className="w-full">
-    <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center mac-w-6xl mx-auto md:space-x-6">
+    <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
       <Image
         src={`https://image.tmdb.org/t/p/original/${
           movie.backdrop_path || movie.poster_path
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-gray-100 p-4 lg:text-lg justify-center gap-6'>
       <NavbarItem title='Trending' param='fetchTrending'/>
-      <NavbarItem title='Top rated' param='fetchTopRated'/>
+      <NavbarItem title='Top Rated' param='fetchTopRated'/>
      
     </div>
   )
