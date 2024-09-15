37b586711adb71c8d1e2fdec0fbeedaaadf253d8
CvlNom <alexkwak24@gmail.com>
2024-09-15 13:56:33 +0900

    create navbar and component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import NavbarItem from "./NavbarItem";
+
+
+export default function Navbar() {
+  return (
+    <div className='flex dark:bg-gray-600 bg-gray-100 p-4 lg:text-lg justify-center gap-6'>
+      <NavbarItem title='Trending' param='fetchTrending'/>
+      <NavbarItem title='Top rated' param='fetchTopRated'/>
+     
+    </div>
+  )
+}
