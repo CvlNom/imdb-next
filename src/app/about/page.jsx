1c477d596f3734a610da6be96478f891ff9805e7
CvlNom <alexkwak24@gmail.com>
2024-09-15 13:22:19 +0900

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 '>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+      A welding wire of the type consists of a plated or uncovered solid wire and a flux-cored wire for carbon steel or stainless steel, wherein one or more of hydrocarbon compounds selected from the group consisting of saturated or unsaturated hydrocarbon compounds, which have from 5 to 12 carbon atoms and a linear or branched structure, and hydro- carbon compounds having a cyclic structure are deposited on a wire surface. A lubricating oil and/or lubricating particles are chemically combined with a wire surface through the one or more hydrocarbon compounds. The total amount of the hydrocarbon compounds, and a lubricating oil and/or lubricating particles deposited on the wire surface is in the range of 0.1 to 5 g per 10 kg of the wire.
+      </p>
+      <p>
+      A copper-free wire having worked faces meaning faces drawn with dies and unworked faces continued in the circumferential direction on a wire surface, the copper-free wire has LD/LT ranging from 0.51 to 0.84 and LY ranging from 10 to 40 um when measurement lines are drawn at every 5 um in a width direction of a measurement area (10,00 um'=100 mx100 um) on the wire surface about 4 wire faces, in which each of the measurement straight lines having a length in the circumferential direction includes at least two worked faces and unworked faces, respectively, in the measurement area, and its starting point and termination point which are al positioned on contact points of the worked faces and the unworked faces.
+      </p>
+      <p>
+      Disclosed is a solid wire for are welding, including: a flat portion of a wire surface with no copper plating, wherein micro Vickers hardness (Hv) of the flat portion is ni the range of 115 to 310 and arithmetical average roughness (Ra) of the flat portion is in the range of 0.01 to 0.30 m. The solid wire is based on anew concept different from a conventional solid wire. The solid wire for are welding has excellent feedability without copper plating, thereby improving weld workability. Consumption of energy during manufacture of the solid wire can be reduced because the solid wire is manufactured without copper plating, and a weld work environment can be improved because copper fumes are not generated.
+      </p>
+    </div>
+  )
+}
